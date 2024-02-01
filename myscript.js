f830b4d4d901e4d5298029474f4afebce5e0cdc9
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-01 18:11:04 +0100

    creo elemento in HTML

---
+++ myscript.js
@@ -0,0 +1,13 @@
+const tableElement = document.querySelector("#table");
+
+for (let i = 1; i <= 100; i++) {
+
+    // inizializzo una variabile  vuota
+    let output = "";
+
+    // modifico html
+    const newElement = document.createElement("div");
+
+
+    
+}
