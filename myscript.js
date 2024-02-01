5ab82a28226955e450a6ada76f3f080c84d7cd2f
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-01 18:32:23 +0100

    esercizio finito

--- myscript.js
+++ myscript.js
@@ -9,5 +9,39 @@ for (let i = 1; i <= 100; i++) {
     const newElement = document.createElement("div");
 
 
+
+    if(i % 3 == 0) {
+
+        output = "Fizz";
+
+        // modifico la classe
+        newElement.className = "fizz ";
+
+    }
     
+    // per i multipli di 5, AGGIUNGO  la parola "Buzz"
+   
+    if (i % 5 == 0) {   
+
+        output += "Buzz";
+
+        // modifico la classe
+        newElement.className += "buzz ";
+    } 
+    
+    
+    if(output == "") {
+
+        output = i;
+
+    }
+
+    console.log(output);
+
+    newElement.innerText = output;
+
+    //aggiungo una classe 
+    newElement.className += "square";
+
+    tableElement.append(newElement);
 }
